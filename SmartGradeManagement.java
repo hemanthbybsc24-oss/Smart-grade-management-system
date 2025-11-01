9b3e74eae20b3b00a9bf5506c980fdf144306fee
hemanthbybsc24-oss <hemanthbybsc24@rvu.edu.in>
2025-11-01 18:02:44 +0530

    adding input method in SmartGradeManagement

--- SmartGradeManagement.java
+++ SmartGradeManagement.java
@@ -7,6 +7,9 @@ System.out.print("\nEnter name of Student " + (i + 1) + ": ");
     String name = sc.next();
             System.out.print("Enter number of subjects: ");
             int sub = sc.nextInt();
+    students[i] = new Student(name, sub);
+            students[i].inputMarks(sc);
+        }
     }
     }
 }
