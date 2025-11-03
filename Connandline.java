31c8bb369a82d4d476b2f29af976aff4dbaac300
Dakshith Saraf <dakshithsarafbsc24@rvu.edu.in>
2025-11-03 11:40:35 +0530

    Add files via upload

---
+++ Connandline.java
@@ -0,0 +1,9 @@
+class Connandline {
+    public static void main(String args[]) {
+        System.out.println("The number of arguments are: " + args.length);
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + i + ": " + args[i]);
+        }
+    }
+}
