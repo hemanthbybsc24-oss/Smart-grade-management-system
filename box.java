59706931630602f4997b6915099bd836f2b53d1a
Dakshith Saraf <dakshithsarafbsc24@rvu.edu.in>
2025-11-03 11:42:56 +0530

    Add files via upload

--- box.java
+++ box.java
@@ -1,18 +1,19 @@
 class box {
-    int h,w,l;
-    int area() {
-        System.out.println("Area of box: " + (h*w*l));
-        int a= 5;
-        return a;
-    }
+    int h;
+    int w;
+    int l;
 
-public static void main(String args[]) 
-{
-    box b1 = new box();
-    b1.h = 10; b1.w = 20; b1.l = 15;
-    box b2 = new box();
-    b2.h = 5; b2.w = 10; b2.l = 12;
-    System.out.println("Box Dimensions: " + b1.h  +  b1.w  +  b1.l);
-    System.out.println("Box Dimensions: " + b2.h  +  b2.w  +  b2.l);
-}
+    public static void main(String args[]){
+        box b1 = new box();
+        b1.h=10;
+        b1.w=20;
+        b1.l=15;
+        System.out.println(" Box-1 Dimensions " +  b1.h  +  b1.w  +  b1.l);
+
+        box b2 = new box();
+        b2.h=20;
+        b2.w=25;
+        b2.l=10;
+        System.out.println(" Box-2 Dimensions " +  b2.h  +  b2.w  +  b2.l);
+    }
 }
